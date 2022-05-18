15a36fee6cc0838d33c915114dd6d2d4661624f8
unknown <shahrukhamin27@gmail.com>
2022-05-19 02:07:05 +0600

    all 4 crud operation api done and used to update DB too

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express= require('express');
+const express = require('express');
 const cors=require('cors')
 require('dotenv').config()
 const port=process.env.PORT || 5000;
@@ -33,12 +33,29 @@ async function run() {
 
       //getting single service using id(find a document)
       app.get('/service/:id',async(req,res)=>{
-          const id =req.params.id
+          const serviceid =req.params.id
           //ai query ar opor base korei next a data get hobey
-          const query={_id:ObjectId(id)}
+          const query={_id:ObjectId(serviceid)}
           //remember jehotu ekta korey item find korbo tai cursor use kora lagbey na
           const service=await serviceCollection.findOne(query) //query ar moddhey id given so oi id basis a ek ekta korey service serviceCollection thekey niye ashbey
           res.send(service)
+      })
+
+      //service post to DB
+      app.post('/service',async(req,res)=>{
+        const newService=req.body; //client side thekey jei data ashbey seita store hobey newService a
+        console.log(newService)
+        const result = await serviceCollection.insertOne(newService);
+        res.send(result)
+      })
+
+      //specific service delete api(service id dhorey dhorey delete)
+      app.delete('/service/:id',async(req,res)=>{
+        const id=req.params.id
+
+        const query = {_id:ObjectId(id)}  //_id ar basis a database thekey ber kora lagbey kakey delete kortey chai
+        const result = await serviceCollection.deleteOne(query);
+        res.send(result);  //ai api je call korbey takey {res.send(result)}aita response hisabey dibo
       })
     } finally {
       
