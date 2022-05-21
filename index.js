7091153ac269360360d8f9752f30e1c0dad19a9d
devshaik444 <mamin172025@bsces.uiu.ac.bd>
2022-05-21 21:46:30 +0600

    jwt work remaining

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors=require('cors')
+const jwt=require('jsonwebtoken')
 require('dotenv').config()
 const port=process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -20,7 +21,18 @@ async function run() {
       //connect to client
       await client.connect();
       const serviceCollection = client.db('genuisCar').collection('service');
+      const orderCollection=client.db('genuisCar').collection('order')
 
+      //AUTH API
+      app.post('/login',async(req,res)=>{
+        const user=req.body //user info get from client side
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: '1d'
+      });
+      res.send({accessToken})
+      })
+
+      //SERVICES API
       //getting all the service from mongodb 'service' collection and show on the server side url(http://localhost:5000/service)
       app.get('/service',async(req,res)=>{
         const query={}
@@ -57,6 +69,32 @@ async function run() {
         const result = await serviceCollection.deleteOne(query);
         res.send(result);  //ai api je call korbey takey {res.send(result)}aita response hisabey dibo
       })
+
+
+      //order collection api (POST API)
+      app.post('/order',async(req,res)=>{
+        const order=req.body;
+        const result=await orderCollection.insertOne(order)
+        res.send(result)
+      })
+      
+
+      //ORDERS API
+      //orders get api
+      app.get('/order',async(req,res)=>{
+        const user_email=req.query.email;         //client side thekey jodi url a kono query/condition dei sheita get kora jay aibhabey
+        console.log(user_email)
+
+        //query null rakhtey pari bcz we want to get all the orders from DB
+        const query={email:user_email}  //DB ar email field ar moddhey user_email jar jar asey tader select korbey             
+
+        //cursor geniusCar collection ar ekta pointer find out korsey 
+        const cursor=orderCollection.find(query)      //single kicho find kortey chailey findone use korbo
+        //found pointer k array tey convert korey felbo
+        const orders=await cursor.toArray()
+        res.send(orders)
+      })
+
     } finally {
       
     }
